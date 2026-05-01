e753a1ed9f7553f6b07cab59cf4443137ff59759
Jackson Clack <clack69857@students.apsb.org>
2026-05-01 18:08:12 -0500

    Basic formatting for control structures

--- src/Task.java
+++ src/Task.java
@@ -1,22 +1,22 @@
 import java.util.ArrayList;
-public class Task {
+public class Task{
     private String taskName;
     private final ArrayList<Double> scoreCollection = new ArrayList<>();
 
-    public Task(String taskName, double urgencyScore) {
+    public Task(String taskName, double urgencyScore){
         this.taskName = taskName;
         scoreCollection.add(urgencyScore);
     }
 
-    public double calculatePriorityRanking() {
+    public double calculatePriorityRanking(){
         double priorityRanking = 0.0;
-        for (Double num : scoreCollection) {
+        for (Double num : scoreCollection){
             priorityRanking += num;
         }
         return priorityRanking;
     }
 
-    public String getTaskName() {
+    public String getTaskName(){
         return taskName;
     }
 }
